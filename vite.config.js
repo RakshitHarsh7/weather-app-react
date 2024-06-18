d36ce546273f9d5844de905e57b8db18b459708a
Rakshit Harsh <rakshitharsh7@gmail.com>
2024-06-18 17:37:22 +0530

    Update background color

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-app-react/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure the output directory is set to 'build'
+  },
 })
